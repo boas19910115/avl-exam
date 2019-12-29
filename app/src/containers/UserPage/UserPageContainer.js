1b1236cfe7d13f36742a46d09630af752a70e256
Yuehnan,Wu <yuehnan.wu@imersive.com>
2019-12-29 08:51:49 +0800

    User information page

---
+++ app/src/containers/UserPage/UserPageContainer.js
@@ -0,0 +1,35 @@
+import React, { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+
+const UserPageContainer = () => {
+  const { user } = useSelector(state => state)
+  console.log(user)
+  const { isData, photoURL, ...restInfo } = useMemo(() => {
+    try {
+      const {
+        providerData: [providerDataFisrt],
+      } = user
+      const { photoURL, ...rest } = providerDataFisrt
+      return { photoURL, ...rest, isData: true }
+    } catch (error) {
+      return { isData: false }
+    }
+  }, [user])
+  return (
+    isData && (
+      <div className='UserPageContainer'>
+        <img src={photoURL} alt='profile' />
+        {Object.keys(restInfo).map(k => {
+          return (
+            <div key={k} className='div-row'>
+              <span>{k}</span>
+              <span>{restInfo[k] || 'unset'}</span>
+            </div>
+          )
+        })}
+      </div>
+    )
+  )
+}
+
+export default UserPageContainer
---
+++ app/src/containers/UserPage/index.js
@@ -0,0 +1,2 @@
+import './userPage.style.scss'
+export { default as UserPage } from './UserPageContainer'
